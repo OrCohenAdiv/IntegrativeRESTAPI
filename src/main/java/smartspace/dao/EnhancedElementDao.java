493713c418d6e107db24ae675269f0d1719c521f
liorb <liorb@liorbarak.lan>
2019-05-06 19:51:18 +0300

    change "Message" to "Element"

--- src/main/java/smartspace/dao/EnhancedElementDao.java
+++ src/main/java/smartspace/dao/EnhancedElementDao.java
@@ -7,6 +7,6 @@ import smartspace.data.ElementEntity;
 public interface EnhancedElementDao<Key> extends ElementDao<Key> {
 	public List<ElementEntity> readAll(int size, int page);
 	public List<ElementEntity> readAll(String sortBy, int size, int page);
-	public List<ElementEntity> readMessageWithSmartspaceContaining (String text, int size, int page);
+	public List<ElementEntity> readElementWithSmartspaceContaining (String text, int size, int page);
 	public ElementEntity importElement(ElementEntity elementEntity);
 }
